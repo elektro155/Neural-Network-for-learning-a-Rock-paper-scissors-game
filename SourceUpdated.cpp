7a752862912a274a28eb7577b46ca7cbf08a5645
Rafal <rafal.laskawski@gmail.com>
2017-09-15 21:38:52 +0200

    Update SourceUpdated.cpp

--- SourceUpdated.cpp
+++ SourceUpdated.cpp
@@ -70,49 +70,49 @@ public:
 		{
 		case 0:{
 			cout << "  |  " << endl;
-			cout << "     " << endl;
+			cout << "  0  " << endl;
 			cout << "     " << endl;
 			break;
 		}
 		case 1:{
 			cout << "    /" << endl;
-			cout << "     " << endl;
+			cout << "  0  " << endl;
 			cout << "     " << endl;
 			break;
 		}
 		case 2:{
 			cout << "     " << endl;
-			cout << "   --" << endl;
+			cout << "  0--" << endl;
 			cout << "     " << endl;
 			break;
 		}
 		case 3:{
 			cout << "     " << endl;
-			cout << "     " << endl;
+			cout << "  0  " << endl;
 			cout << "    \\" << endl;
 			break;
 		}
 		case 4:{
 			cout << "     " << endl;
-			cout << "     " << endl;
+			cout << "  0  " << endl;
 			cout << "  |  " << endl;
 			break;
 		}
 		case 5:{
 			cout << "     " << endl;
-			cout << "     " << endl;
+			cout << "  0  " << endl;
 			cout << "/    " << endl;
 			break;
 		}
 		case 6:{
 			cout << "     " << endl;
-			cout << "--   " << endl;
+			cout << "--0  " << endl;
 			cout << "     " << endl;
 			break;
 		}
 		case 7:{
 			cout << "\\    " << endl;
-			cout << "     " << endl;
+			cout << "  0  " << endl;
 			cout << "     " << endl;
 			break;
 		}
